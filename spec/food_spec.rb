e1e485ddb7e83dc8728d5bfaf002009d6c2f2176
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-09 11:05:56 +0000

    Creada pruebas para comprobar si existe un método para obtener los carbohidratos y si se almacenan correctamente

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -27,4 +27,16 @@ RSpec.describe Food do
 			expect(@carne_vaca.proteins).to eq(21.1)
 		end
 	end
+
+	describe "# Atributo Carbohydrates" do
+	
+		it "# Existe el atributo carbohydrates" do
+			expect(@carne_vaca).to respond_to(:carbohydrates)
+		end
+		
+		it "# Se almacena correctamente el valor de las proteinas" do
+			expect(@carne_vaca.carbohydrates).to eq(0.0)
+		end
+
+	end
 end
