e87079914fbb9af7cd3c68ee87c986027c9dffc1
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-16 14:32:45 +0000

    Creado código para obtener el elemento tail de la lista

--- lib/food/list.rb
+++ lib/food/list.rb
@@ -2,9 +2,10 @@ module Food
 	
 	Node = Struct.new(:value, :next, :prev)
 	class List
-		attr_reader :head
+		attr_reader :head, :tail
 		def initialize(value)
 			@head = Node.new(value, nil, nil)
+			@tail = @head
 		end
 	end
 end
