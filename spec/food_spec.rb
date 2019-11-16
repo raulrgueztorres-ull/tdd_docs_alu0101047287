d3b9bd71ad0215ab78598899dc14bb0b8e9f10fd
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-16 17:24:52 +0000

    Creada la prueba para calcular el uso del terreno de la dieta española

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -292,6 +292,10 @@ RSpec.describe Food do
 		it "# Se calcula correctamente la media de gases de la dieta española para un año" do
 			expect(@menu.list_environmental_impact_annual(@española)).to eq(16279)
 		end	
+		
+		it "# Se calcula correctamente el uso del terreno de la dieta española" do
+			expect(@menu.list_ground_use(@española)).to eq(214)
+		end
 	end
 
 end
