7720ca5270bd010108ce5d8cb33f6f203af8d945
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-17 20:07:24 +0000

    Creado método list_amoun_environmental_impact_annual para calcular la media de gases anual con cantidades

--- lib/food/food.rb
+++ lib/food/food.rb
@@ -107,6 +107,25 @@ module Food
 			return (impact_sum * 365).round(2)
 		end
 
+		def list_amount_environmental_impact_annual(values, amount)
+			impact_sum = 0
+			current_node = values.tail
+			i = 0
+			if values.length == amount.length
+				while current_node != values.head
+					impact_sum += (current_node.value.gei * amount[i]).to_f
+					current_node = current_node.next
+					i += 1
+				end
+				if current_node == values.head
+					impact_sum += (current_node.value.gei * amount[i]).to_f
+				end
+			else
+				raise "Error. Los alimentos pasados y sus cantidades no son iguales."
+			end
+			return (impact_sum * 365).round(2)
+		end
+
 		def list_ground_use(values)
 			ground_sum = 0
 			current_node = values.head
