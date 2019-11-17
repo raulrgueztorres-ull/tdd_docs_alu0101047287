abca6828194fab63c79917af3b63e11a1c8c0e45
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-17 20:14:15 +0000

    Creada la prueba para calcular el uso del terreno de la dieta española usando cantidades para un año

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -316,6 +316,10 @@ RSpec.describe Food do
 		it "# Se calcula correctamente el uso del terreno de la dieta española con cantidades" do
 			expect(@menu.list_amount_ground_use(@española_complex, @cantidad_española)).to eq(214)
 		end
+
+		it "# Se calcula correctamente el uso del terreno de la dieta española con cantidades para un año" do
+			expect(@menu.list_amount_ground_use_annual(@española_complex, @cantidad_española)).to eq(78110)
+		end
 	end
 
 end
