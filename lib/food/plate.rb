751a73030fcd6d1e532642da54b7ac1b53c994ba
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-24 16:10:45 +0000

    Generado código para obtener correctamente el porcentaje de proteinas del conjunto de alimentos del plato

--- lib/food/plate.rb
+++ lib/food/plate.rb
@@ -4,6 +4,27 @@ module Food
         def initialize(name, food, food_gr)
             @name, @food, @food_gr = name, food, food_gr
         end
+
+        def kcals_plate()
+            kcals = 0
+            i = 0
+            food.each { |val|
+                kcals += val.value.energetic_value_complex(val.value.proteins * (food_gr[i]/100), val.value.carbohydrates * (food_gr[i]/100), val.value.lipids * (food_gr[i]/100))
+                i += 1
+            }
+            return kcals
+        end
+
+        def percentage_proteins()
+            kcals = kcals_plate
+            proteins = 0
+            i = 0
+            food.each { |val|
+                proteins += val.value.kcal_proteins(val.value.proteins * (food_gr[i]/100))
+                i += 1
+            }
+            return ((proteins * 100)/kcals).round(2)
+        end
     end
 
 
