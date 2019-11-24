8c85bb1abb4d428e33bc7aca49d6f14522bcba14
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-24 15:21:44 +0000

    Creado fichero plate.rb con la clase Plate y añadido require en food.rb

--- lib/food.rb
+++ lib/food.rb
@@ -1,6 +1,7 @@
 require "food/version"
 require "food/food"
 require "food/list"
+require "food/plate"
 module Food
   class Error < StandardError; end
   # Your code goes here...
---
+++ lib/food/plate.rb
@@ -0,0 +1,7 @@
+module Food
+    class Plate
+
+    end
+
+
+end
