876434ee3a7bf7342eb226c54280b0f75bdc3c44
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-25 13:10:48 +0000

    Generado código para obtener correctamente el uso del terreno de un plato con la clase PlateAmbiental

--- lib/food/plateAmbiental.rb
+++ lib/food/plateAmbiental.rb
@@ -6,12 +6,19 @@ module Food
         def initialize(name, food, food_gr)
             super(name, food, food_gr)
             @co2 = daily_co2
+            @m2 = ground_use_plate
         end
 
         def daily_co2
             array = []
             food_gr.each { |val| array.push(val/100) }
             food.tail.value.list_amount_environmental_impact(food, array)
+        end
+
+        def ground_use_plate
+            array = [] 
+            food_gr.each { |val| array.push(val/100) }
+            food.tail.value.list_amount_ground_use(food, array)
         end
     end
 end
