6260d96cd63508f14e5602b084a464ae5ad95004
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-11-25 13:06:15 +0000

    Generado código para obtener correctamente la emisión de gases de efecto invernadero de un plato con la clase PlateAmbiental

--- lib/food/plateAmbiental.rb
+++ lib/food/plateAmbiental.rb
@@ -3,8 +3,15 @@ module Food
     class PlateAmbiental < Plate
 
         attr_reader :co2, :m2
-        def initialize(name, food, food_gr, co2 = 0, m2 = 0)
+        def initialize(name, food, food_gr)
             super(name, food, food_gr)
+            @co2 = daily_co2
+        end
+
+        def daily_co2
+            array = []
+            food_gr.each { |val| array.push(val/100) }
+            food.tail.value.list_amount_environmental_impact(food, array)
         end
     end
 end
