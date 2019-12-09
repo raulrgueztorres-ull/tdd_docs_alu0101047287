6c5010e1fa8519854ef4a3e782d41bb202082931
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-12-09 19:16:36 +0000

    Generado código para incrementar el precio a través de un array

--- lib/food/array.rb
+++ lib/food/array.rb
@@ -2,4 +2,19 @@ class Array
     def maxo
         self.max { |a, b| a.huella_nutricional <=> b.huella_nutricional }
     end
+    
+    def incremento_precio(plates)
+        max_huella = plates.maxo.huella_nutricional
+        new_array = []
+        self.each { |val| 
+            if max_huella == 3
+                new_array.push(val * 1.50)
+            elsif max_huella == 2 
+                new_array.push(val * 1.25)
+            else
+                new_array.push(val * 1.10)
+            end
+        }
+        return new_array
+    end 
 end
