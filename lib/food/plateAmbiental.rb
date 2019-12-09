ead61873f13d46fcb45dff19ab6c313560c874dc
Raul Rodriguez Torres <alu0101047287@ull.edu.es>
2019-12-09 20:37:47 +0000

    Comentada la clase PlateAmbiental

--- lib/food/plateAmbiental.rb
+++ lib/food/plateAmbiental.rb
@@ -1,34 +1,48 @@
 module Food
-
+    # Clase PlateAmbiental. Esta clase hereda de la clase Plate.
     class PlateAmbiental < Plate
-
+        # Definicion de los atributos de lectura para co2 y m2 
         attr_reader :co2, :m2
+        # Incluye Comparable
         include Comparable
+        #Incluye Enumerable
         include Enumerable
 
+        # Método para la creación de un plato ambiental.
+        # @param name [String] Nombre del plato.
+        # @param food [List] Lista de alimentos del plato.
+        # @param food_gr [Array] Array de gramos de cada alimento del plato.
+        # @return [PlateAmbiental] Objeto de clase PlateAmbiental.
         def initialize(name, food, food_gr)
             super(name, food, food_gr)
             @co2 = daily_co2
             @m2 = ground_use_plate
         end
 
+        # Método para el correcto funcionamiento de Comparable
+        # @param other [PlateAmbiental] Segundo plato a comparar.
         def <=> (other)
             co2 <=> other.co2
-           #huella_nutricional <=> other.huella_nutricional
         end
         
+        # Método para calcular las emisiones diarias de co2 de un plato
+        # @return [Numeric] Valor de emisiones de co2 del plato.
         def daily_co2
             array = []
             food_gr.each { |val| array.push(val/100) }
             food.tail.value.list_amount_environmental_impact(food, array)
         end
 
+        # Método para calcular el uso del terreno en m2 del plato.
+        # @return [Numeric] Valor de uso del terreno del plato.
         def ground_use_plate
             array = [] 
             food_gr.each { |val| array.push(val/100) }
             food.tail.value.list_amount_ground_use(food, array)
         end
         
+        # Método para calcular el índice del valor calórico del plato en función de un rango.
+        # @return [Numeric] Índice del valor calórico total del plato.
         def index_VCT
             if VCT() < 670
                 return 1
@@ -39,6 +53,8 @@ module Food
             end
         end
 
+        # Método para calcular el índice de GEI del plato en función de un rango.
+        # @return [Numeric] Índice del GEI del plato.
         def index_GEI
             if daily_co2 < 800
                 return 1
@@ -49,10 +65,14 @@ module Food
             end
         end
         
+        # Método para calcular el índice de la huella nutricional del plato.
+        # @return [Numeric] Índice de la huella nutricional del plato.
         def huella_nutricional() 
             ((index_VCT + index_GEI)/2).round(1)
         end
 
+        # Método para formatear un plato
+        # @return [String] Cadena con la descripción del plato.
         def to_s
             s = super.to_s
             s << ", Impacto: GEI de #{co2} kgCO2eq y Uso del terreno de #{m2} metros cuadrados" 
